939814b93ae3a4732e78e176c80a23ff439bca7c
Ryan <ryan.mwood@outlook.com>
2020-06-10 12:02:22 -0700

    Added color characteristic for testing with app integration

--- LEDControllerproto/LEDControllerproto.ino
+++ LEDControllerproto/LEDControllerproto.ino
@@ -1,7 +1,12 @@
 #include <ArduinoBLE.h>
+#include <Adafruit_NeoPixel.h>
 
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(3, 2 , NEO_GRB + NEO_KHZ800);
+BLEService colorService("7f453f59-6477-40d8-8054-f5526953986a");
 
 void setup() {
+  pixels.begin();
+  pixels.setBrightness(50);
   if (!BLE.begin()) {
     Serial.println("Failed to start BLE module");
     while (1);
@@ -15,6 +20,13 @@ void setup() {
   BLE.setEventHandler(BLEDisconnected, noConnectionHandler);
   BLE.setLocalName("LED");
   BLE.setDeviceName("LED");
+  BLEIntCharacteristic colorSetInt("7f453f59-6477-40d8-8054-f5526953986a", BLERead | BLEWrite);
+  colorService.addCharacteristic(colorSetInt);
+  colorSetInt.setEventHandler(BLEWritten, colorCharacteristicWritten);
+  BLE.addService(colorService);
+  BLE.setAdvertisedService(colorService);
+  
+  
   BLE.advertise();
   
 }
@@ -28,8 +40,27 @@ void loop() {
 
 void periphConnectedHandler(BLEDevice central) {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level) 
+  pixels.setPixelColor(0, pixels.Color(100,100,100));
+  pixels.setPixelColor(1, pixels.Color(0,0,55));
+  pixels.setPixelColor(2, pixels.Color(254,254,254));
+  pixels.show(); 
 }
 
 void noConnectionHandler(BLEDevice central) {
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  pixels.clear();
+  pixels.show();
+}
+
+void colorCharacteristicWritten(BLEDevice central, BLECharacteristic characteristic) {
+    
+    pixels.setPixelColor(1, pixels.Color(100,0,100));
+    pixels.setPixelColor(2, pixels.Color(100,0,100));
+    byte charRead[3];
+    characteristic.readValue(charRead, 3);
+    Serial.println(charRead[0]);
+    Serial.println(charRead[1]);
+    Serial.println(charRead[2]);
+    pixels.setPixelColor(0, pixels.Color(charRead[0],charRead[1],charRead[2]));
+    pixels.show();
 }
